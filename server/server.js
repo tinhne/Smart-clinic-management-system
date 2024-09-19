2b2f8e65c8aab1e5d484c4cfc2dc55ddcf523644
tandung10 <nguyentandung20031011@gmail.com>
2024-09-19 21:48:31 +0700

    install nodemon

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const connectDB = require('./config/mongoDB')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/mongoDB");
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cors())
+app.use(cors());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`http://localhost:${PORT}`);
+});

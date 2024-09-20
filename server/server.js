991721cdf66b5b53911146890331375297194640
ntn1105 <130301312+ntn1105@users.noreply.github.com>
2024-09-20 12:57:20 +0700

    config project

--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
+require('dotenv').config();
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/mongoDB");
-
-dotenv.config();
-
-connectDB();
-
+const mongoose = require("mongoose");
+const connectDB = require("./config/mongoDB"); // Đảm bảo file này tồn tại
 const app = express();
+const port = process.env.PORT || 8888;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// config req.body
+app.use(express.json()); // for json
+app.use(express.urlencoded({ extended: true })); // for form data
 
-app.use(cors());
+// Start server and connect to MongoDB
+(async () => {
+    try {
+        // Kết nối MongoDB
+        await connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+        // Lắng nghe port
+        app.listen(port, () => {
+            console.log(`Backend Node.js App listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log(">>> Error connect to DB: ", error);
+    }
+})();

fedb9deef8807fffcb5becde16262afd7c4a54a2
tandung10 <nguyentandung20031011@gmail.com>
2024-09-30 18:46:48 +0700

    ..

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "doctor", "patient"],
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[0-9]{10,15}$/,
+    },
+    address: {
+      type: String,
+      maxlength: 100,
+    },
+    birthdate: {
+      type: Date,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+// Hash password trước khi lưu
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);

a29f93cbe06e4510c52995678177d148e11a6610
tandung10 <nguyentandung20031011@gmail.com>
2024-10-06 21:06:10 +0700

    crud thuoc

---
+++ server/routes/medicineRoutes.js
@@ -0,0 +1,35 @@
+const {
+  createMedicine,
+  getAllMedicines,
+  updateMedicine,
+  deleteMedicine,
+} = require("../controllers/medicineController");
+const express = require("express");
+const { authenticate } = require("../middlewares/authenticate");
+const { authorize } = require("../middlewares/authorize");
+const router = express.Router();
+
+router.post(
+  "/create-medicine",
+  authenticate,
+  authorize(["admin"]),
+  createMedicine
+);
+
+router.get("/all-medicines", authenticate, getAllMedicines);
+
+router.put(
+  "/update-medicine/:id",
+  authenticate,
+  authorize(["admin"]),
+  updateMedicine
+);
+
+router.delete(
+  "/delete-medicine/:id",
+  authenticate,
+  authorize(["admin"]),
+  deleteMedicine
+);
+
+module.exports = router;

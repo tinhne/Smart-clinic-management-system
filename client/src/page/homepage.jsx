b5f01687902d051bdf914ab6f2d65f1ee727d135
ntn1105 <130301312+ntn1105@users.noreply.github.com>
2024-10-07 00:05:50 +0700

    homepage countinue

--- client/src/page/homepage.jsx
+++ client/src/page/homepage.jsx
@@ -1,6 +1,9 @@
 import image1 from "../assets/img/image 1.png";
+import avatar from "../assets/img/1237b30268db9ee0c9cbe3a79b1ff8fa.jpg";
 import "../style/homepage.scss";
 import { CiSearch } from "react-icons/ci";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const HomePage = () => {
   return (
@@ -13,7 +16,6 @@ const HomePage = () => {
             Pandora để có số thứ tự và khung giờ khám trước.
           </p>
 
-          {/* Container for input and search icon */}
           <div className="search-container">
             <input
               type="text"
@@ -23,7 +25,123 @@ const HomePage = () => {
             <CiSearch className="search-icon" />
           </div>
         </div>
-        <img src={image1} alt="Mô tả hình ảnh" className="img-home-page" />
+
+        <img src={image1} className="img-home-page" />
+      </div>
+      <div className="booking_container">
+        <div className="booking_header">
+          <p className="title">Đặt lịch phòng khám trực tuyến</p>
+          <p className="description">
+            Tìm bác sĩ chính xác - Đặt lịch khám dễ dàng
+          </p>
+        </div>
+        <div className="booking_doctor">
+          <div className="action">
+            <div>
+              <p className="booking_title">Đặt khám bác sĩ</p>
+              <p className="booking_content">
+                Phiếu khám kèm số thứ tự và thời gian của bạn được xác nhận
+              </p>
+            </div>
+            <div>
+              <button className="view-more-button">Xem Thêm &gt;</button>
+            </div>
+          </div>
+          <div className="doctor-cards">
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+            <div className="doctor-card">
+              <div className="doctor-image">
+                <img src={avatar} alt="Doctor Avatar" />
+              </div>
+              <div className="doctor-info">
+                <h3>BS. CK1 Nguyễn Thành Nhân</h3>
+                <p>Tai Mũi họng</p>
+                <p>Bệnh viện Y Huế</p>
+              </div>
+              <div className="doctor-action">
+                <button>Đặt lịch khám</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

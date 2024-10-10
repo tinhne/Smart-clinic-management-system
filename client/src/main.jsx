3e019225e5551e0618c14250a215637d3b736ead
ntn1105 <130301312+ntn1105@users.noreply.github.com>
2024-10-10 12:56:42 +0700

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,19 +3,51 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/users/homepage.jsx";
+import AdminApp from "../AdminApp.jsx";
+import Dashboard from "./pages/admin/Dashboard.jsx";
+import Doctors from "./pages/admin/Doctors.jsx";
+import Patients from "./pages/admin/Patients.jsx";
+import Medication from "./pages/admin/Medication.jsx";
+import Services from "./pages/admin/Services.jsx";
 const router = createBrowserRouter([
   {
     path: "",
-    element: <App />,
+    element: <App></App>,
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: <HomePage></HomePage>,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminApp />,
+    children: [
+      {
+        index: true,
+        path: "dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+      {
+        path: "doctors",
+        element: <Doctors></Doctors>,
+      },
+      {
+        path: "patients",
+        element: <Patients></Patients>,
+      },
+      {
+        path: "medicine",
+        element: <Medication></Medication>,
+      },
+      {
+        path: "dich-vu-kham",
+        element: <Services></Services>,
       },
     ],
   },
 ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />

77c29a43536b82a779a41a250b9207f0350be992
tandung10 <nguyentandung20031011@gmail.com>
2024-10-12 00:05:19 +0700

    ngăn chặn chuyển trang khi chưa đăng nhập

--- client/src/components/Private/PrivateRoute.jsx
+++ client/src/components/Private/PrivateRoute.jsx
@@ -3,8 +3,11 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const token = localStorage.getItem("access_token"); // Check for token in localStorage
-
-  return token ? <Outlet /> : <Navigate to="/admin/login" />; // Redirect to login if not authenticated
+  const role = localStorage.getItem("role");
+  if (!token || role !== "admin") {
+    return <Navigate to="/admin/login" />;
+  }
+  return <Outlet />;
 };
 
 export default PrivateRoute;

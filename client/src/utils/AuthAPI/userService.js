6b76d6818b1d2377bd0b56c3c61ea772cb531ec8
tinhne <levantrungtinh13@gmail.com>
2024-10-24 21:25:05 +0700

    change-password

--- client/src/utils/AuthAPI/userService.js
+++ client/src/utils/AuthAPI/userService.js
@@ -1,8 +1,12 @@
 import axios from "../../config/axios.customize";
 
-export const  editUser = async (userUD, formData) => {
-    return axios.put(`/api/user/edit-users/${userUD}`, formData)
+export const  editUser = async (userID, formData) => {
+    return axios.put(`/api/user/edit-users/${userID}`, formData)
 }
 export const getUserById = async (userID, role) => {
     return axios.get(`/api/user/users/${userID}?role=${role}`)
+}
+// Cập nhật hàm changePassword
+export const changePassword = async (currentPassword, newPassword) => {
+    return axios.put('/api/auth/change-password', { currentPassword, newPassword});
 }

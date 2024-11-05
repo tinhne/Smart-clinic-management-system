2b51310221782cad018b40c8bc7e19231e76ee4e
tandung10 <nguyentandung20031011@gmail.com>
2024-11-05 18:32:36 +0700

    chỉnh css, cập nhật lại API Blog

--- server/service/BlogService.js
+++ server/service/BlogService.js
@@ -1,35 +1,55 @@
 const Blog = require("../models/Blog");
 
-// Hàm tạo blog
+// Create a new blog
 exports.createBlog = async (blogData, author_id, author_name) => {
-  const newBlog = new Blog({
-    ...blogData,
-    author_id,
-    author_name,
-  });
-
-  await newBlog.save();
-  return newBlog;
+  try {
+    const newBlog = new Blog({ ...blogData, author_id, author_name });
+    return await newBlog.save();
+  } catch (error) {
+    throw new Error(`Error creating blog: ${error.message}`);
+  }
 };
 
-// Hàm lấy tất cả blog
+// Get all blogs
 exports.getAllBlogs = async () => {
-  const blogs = await Blog.find().populate("author_id", "username");
-  return blogs;
+  try {
+    return await Blog.find().populate("author_id", "username");
+  } catch (error) {
+    throw new Error(`Error fetching blogs: ${error.message}`);
+  }
 };
 
-// Hàm lấy blog theo id
+// Get blog by ID
 exports.getBlogById = async (id) => {
-  const blog = await Blog.findById(id).populate("author_id", "username");
-  return blog;
+  try {
+    const blog = await Blog.findById(id).populate("author_id", "username");
+    if (!blog) throw new Error("Blog not found");
+    return blog;
+  } catch (error) {
+    throw new Error(`Error fetching blog: ${error.message}`);
+  }
 };
 
-// Hàm cập nhật blog theo id
+// Update blog by ID
 exports.updateBlogById = async (id, blogData) => {
-  await Blog.findByIdAndUpdate(id, blogData);
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(id, blogData, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedBlog) throw new Error("Blog not found");
+    return updatedBlog;
+  } catch (error) {
+    throw new Error(`Error updating blog: ${error.message}`);
+  }
 };
 
-// Hàm xóa blog theo id
+// Delete blog by ID
 exports.deleteBlogById = async (id) => {
-  await Blog.findByIdAndDelete(id);
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(id);
+    if (!deletedBlog) throw new Error("Blog not found");
+  } catch (error) {
+    throw new Error(`Error deleting blog: ${error.message}`);
+  }
 };

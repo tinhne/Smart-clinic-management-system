aec6165e31db6cb1dc58cb5af16c04ad226ace92
tandung10 <nguyentandung20031011@gmail.com>
2024-12-07 17:14:58 +0700

    ....

--- client/src/pages/users/ModalMedicine.jsx
+++ client/src/pages/users/ModalMedicine.jsx
@@ -1,6 +1,6 @@
-// Modal.js
-import React from 'react';
-import './Modal.scss'; // Bạn có thể tạo file CSS riêng cho modal
+import React from "react";
+import PropTypes from "prop-types";
+import "./Modal.scss";
 
 const Modal = ({ isOpen, onClose, medicine }) => {
   if (!isOpen) return null;
@@ -10,12 +10,27 @@ const Modal = ({ isOpen, onClose, medicine }) => {
       <div className="modal-content_medicine">
         <h2>{medicine.name}</h2>
         <img src={medicine.medicalImage} alt={medicine.name} />
-        <p><strong>Mô tả:</strong> {medicine.description}</p>
-        <p><strong>Giá:</strong> {medicine.price}đ</p>
+        <p>
+          <strong>Mô tả:</strong> {medicine.description}
+        </p>
+        <p>
+          <strong>Giá:</strong> {medicine.price}đ
+        </p>
         <button onClick={onClose}>Đóng</button>
       </div>
     </div>
   );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  medicine: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    medicalImage: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default Modal;
